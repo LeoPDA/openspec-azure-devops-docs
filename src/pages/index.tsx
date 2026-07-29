2b424a9fd27872f7f456fc5c2afc0d9bfa8ba5f0
Leonardo Arouche - AfixCode <leonardo.arouche@afixcode.com.br>
2026-07-29 09:05:46 -0300

    Correção do index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/openpec-azure-devops-guide">
+            to="/docs/openspec-azure-devops-guide">
             Tutorial
           </Link>
         </div>
